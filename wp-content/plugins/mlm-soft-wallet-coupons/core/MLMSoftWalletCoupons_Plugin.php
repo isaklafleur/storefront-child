66a76a52fbc8fb6cfd18c71ddadab6dc454d48ac
apiletskiy <ap10111997.alex@yandex.ru>
2021-04-08 17:01:22 +0700

    Remove custom discounts

--- wp-content/plugins/mlm-soft-wallet-coupons/core/MLMSoftWalletCoupons_Plugin.php
+++ wp-content/plugins/mlm-soft-wallet-coupons/core/MLMSoftWalletCoupons_Plugin.php
@@ -38,15 +38,15 @@ class MLMSoftWalletCoupons_Plugin
         add_action('woocommerce_before_cart', [$this, 'updateUserCoupon'], 10, 0);
         add_action('woocommerce_account_wc-smart-coupons_endpoint', [$this, 'updateUserCoupon'], 10, 0);
 
-        add_filter('woocommerce_coupon_discount_types', [$this, 'crateCustomCouponDiscountType'], 10, 1);
-        add_filter('woocommerce_coupon_custom_discounts_array', [$this, 'calcCustomDiscount'], 10, 2);
-
         add_action('woocommerce_checkout_order_created', [$this, 'performWalletOperationForOrder'], 10, 1);
     }
 
     public function updateUserCoupon()
     {
         $user = wp_get_current_user();
+        if (!$user->ID) {
+            return;
+        }
         $coupon = $this->getUserWalletCoupon($user->ID);
 
         global $MlmSoft;
@@ -57,52 +57,15 @@ class MLMSoftWalletCoupons_Plugin
         }
     }
 
-    public function crateCustomCouponDiscountType($discount_types)
-    {
-        $discount_types['mlmsoft_wallet_discount'] = __('MLM Soft wallet discount', 'woocommerce');
-        return $discount_types;
-    }
-
-    public function calcCustomDiscount($discounts, $coupon)
-    {
-        global $woocommerce;
-        /** @var WC_Cart $cart */
-        $cart = $woocommerce->cart;
-
-        $rbd = RBD_Plugin::getInstance();
-        $rbd_discounts = $rbd->calcDiscounts($cart);
-
-        $totalRbdSum = 0;
-        foreach ($rbd_discounts as $discount) {
-            $totalRbdSum += array_sum($discount['discounts']);
-        }
-
-        $totalCartSum = 0;
-        $cartContents = $cart->get_cart_contents();
-        foreach ($cartContents as $cart_item_key => $cart_item) {
-            $totalCartSum += $cart_item['line_subtotal'];
-        }
-
-        $totalCouponDiscount = floatval(min($totalCartSum - $totalRbdSum, $coupon->get_amount()));
-
-        foreach ($cartContents as $cart_item_key => $cart_item) {
-            if ($totalCouponDiscount == 0) {
-                break;
-            }
-            $subtotal = $cart_item['line_subtotal'];
-            $currentDiscountValue = min($subtotal, $totalCouponDiscount);
-
-            $discounts[$cart_item_key] = wc_add_number_precision($currentDiscountValue);
-            $totalCouponDiscount -= $currentDiscountValue;
-        }
-        return $discounts;
-    }
-
     public function performWalletOperationForOrder($order) {
         $appliedCoupons = $order->get_coupons();
         if ($appliedCoupons) {
             $userId = get_current_user_id();
+            if (!$userId) {
+                return;
+            }
             $userCoupon = $this->getUserWalletCoupon($userId);
+            /** @var  $coupon WC_Order_Item_Coupon */
             foreach ($appliedCoupons as $key => $coupon) {
                 if ($coupon->get_code() == $userCoupon->get_code()) {
                     global $MlmSoft;
@@ -110,7 +73,7 @@ class MLMSoftWalletCoupons_Plugin
                     $walletTypeId = $this->options->get_option_value(MLMSoftWalletCoupons_Options::MLMSOFT_WALLET_TYPE_ID_KEY, 0);
                     $operationTypeId = $this->options->get_option_value(MLMSoftWalletCoupons_Options::MLMSOFT_WALLET_OPERATION_ID_KEY, 0);
 
-                    $discount = floatval($coupon->get_discount());
+                    $discount = floatval($coupon->get_discount()) + floatval($coupon->get_discount_tax());
                     $MlmSoft->addWalletOperation($userId, -$discount, $walletTypeId, $operationTypeId);
                 }
             }
